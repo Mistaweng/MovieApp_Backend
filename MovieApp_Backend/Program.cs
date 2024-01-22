c5b2456bca13f2b34f3fee963e33e4a33d755223
Mistaweng <emmanuelgyang.1822@gmail.com>
2024-01-22 01:41:00 +0100

    Added production variables

--- MovieApp_Backend/Program.cs
+++ MovieApp_Backend/Program.cs
@@ -15,10 +15,16 @@ builder.Services.AddScoped<OmdbService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
+	app.UseDeveloperExceptionPage();
 	app.UseSwagger();
-	app.UseSwaggerUI();
+	app.UseSwaggerUI(c =>
+	{
+		c.SwaggerEndpoint("/swagger/v1/swagger.json", "MovieApp-v1");
+		c.RoutePrefix = "swagger";
+	});
 }
 
 app.UseHttpsRedirection();
